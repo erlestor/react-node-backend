822dd0e738ef34cce198602dd77dab32d8759b38
Erlend Storsve <erlestor@ntnu.no>
2021-09-22 21:47:51 +0200

    opprettet prosjekt

---
+++ server/index.js
@@ -0,0 +1,29 @@
+// setup greier
+const express = require("express")
+
+const PORT = process.env.PORT || 3001
+
+const app = express()
+
+// api endpoints
+app.get("/api", (req, res) => {
+  res.json({ message: "Hello from server!" })
+})
+
+// Have Node serve the files for our built React app
+app.use(express.static(path.resolve(__dirname, "../client/build")))
+
+// Handle GET requests to /api route
+app.get("/api", (req, res) => {
+  res.json({ message: "Hello from server!" })
+})
+
+// All other GET requests not handled before will return our React app
+app.get("*", (req, res) => {
+  res.sendFile(path.resolve(__dirname, "../client/build", "index.html"))
+})
+
+// alltid nederst?
+app.listen(PORT, () => {
+  console.log(`Server listening on ${PORT}`)
+})
